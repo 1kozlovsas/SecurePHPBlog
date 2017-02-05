dd47c08fae30d0a0c093592f3ae921ce48be965a
root <root@debian>
2017-02-05 07:44:59 -0500

    Fixing view-users.php merge conflicts

--- view-users.php
+++ view-users.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 session_start();
 include ('requires/header.php');
@@ -39,43 +38,3 @@ echo '<tr>
 <?php
 include ('requires/footer.php');
 ?>
-=======
-<?php
-include ('requires/header.php');
-?>
-<h1>Pick a user. Any user. I dare you.</h1>
-
-<table>		
-<thead>			
-<tr>				
-	<td>Usernames:</td>			
-</tr>		
-</thead>		
-<tbody>		 
-<?php
-$dbname = "";
-$user = "";
-$password = "";              
-$db = pg_connect('host=localhost dbname='.$dbname.'user='.$user.'password='.$password);             
-$query = "SELECT username FROM users";             
-$result = pg_query($query);            
-if (!$result) {                
-echo "Problem with query " . $query . "<br/>";                
-echo pg_last_error();                            
-}             
-while($row = pg_fetch_assoc($result)) {                
-echo '<tr>                
-<td>                
-<a href="view-posts.php?username='.$row['username'].'">                
-<button type="button" class="btn btn-info">'.$row['username'].'
-<button>                
-</a>            
-</tr>';            
-}            
-?>				
-</tbody>	
-</table>	
-<?php
-include ('requires/footer.php');
-?>
->>>>>>> c35aaaf86b0a57ca175320d8217fef7dabb8e196
