72c6addf3b991724e9de4f3f6892f840e020b351
Alex Kozlov <sashak@cogeco.ca>
2017-02-15 14:36:28 -0800

    Finished view-users.php

--- view-users.php
+++ view-users.php
@@ -1,41 +1,6 @@
-<!DOCTYPE html>
-<html lang = "en">
-<head>	 
-<meta charset="utf-8">        
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">        
-<title>List of Users</title>        
-<meta name="description" content="">        
-<meta name="viewport" content="width=device-width, initial-scale=1">        
-<link rel="apple-touch-icon" href="apple-touch-icon.png">        
-<link rel="stylesheet" href="css/bootstrap.min.css">        
-<style>            
-body {               
-padding-top: 50px;                
-padding-bottom: 20px;            
-}        
-</style>        
-<link rel="stylesheet" href="css/bootstrap-theme.min.css">        
-<link rel="stylesheet" href="css/main.css">        
-<script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>	
-<title>List of Users</title>
-</head>
-<body>
-<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">      
-<div class="container">        
-<div class="navbar-header">          
-<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">            
-<span class="sr-only">Toggle navigation</span>            
-<span class="icon-bar"></span>            
-<span class="icon-bar"></span>            
-<span class="icon-bar"></span>          
-</button>          
-<a class="navbar-brand" href="../index.html">Home page</a>        
-</div>        
-<div id="navbar" class="navbar-collapse collapse">        
-</div><!--/.navbar-collapse -->      
-</div>    
-</nav>
-
+<?php
+readfile ('requires/header.php');
+?>
 <h1>Pick a user. Any user. I dare you.</h1>
 
 <table>		
@@ -45,18 +10,21 @@ padding-bottom: 20px;
 </tr>		
 </thead>		
 <tbody>		 
-<?php              
-$db = pg_connect('host=localhost dbname= user= password=');             
+<?php
+$dbname = "test1";
+$user = "test1";
+$password = "1kozlovsas";              
+$db = pg_connect('host=localhost dbname='.$dbname.'user='.$user.'password='.$password);             
+$query = "SELECT username FROM users";             
 $result = pg_query($query);            
 if (!$result) {                
 echo "Problem with query " . $query . "<br/>";                
-echo pg_last_error();                
-exit();            
+echo pg_last_error();                            
 }             
 while($row = pg_fetch_assoc($result)) {                
 echo '<tr>                
 <td>                
-<a href="userdetails.php?username='.$row['username'].'">                
+<a href="view-posts.php?username='.$row['username'].'">                
 <button type="button" class="btn btn-info">'.$row['username'].'
 <button>                
 </a>            
@@ -65,5 +33,6 @@ echo '<tr>
 ?>				
 </tbody>	
 </table>	
-</body>
-</html>
+<?php
+readfile ('requires/footer.php');
+?>
