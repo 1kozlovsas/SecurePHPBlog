2f10684af0d1327779513c31335251a98ca27d93
root <root@SCOOBY.localdomain>
2017-02-16 12:11:49 -0500

    Updating layout of manage-posts.php

--- manage-posts.php
+++ manage-posts.php
@@ -9,7 +9,7 @@ $_SESSION['old_page'] = '';
 
 <center><h2>
 <a href="new-post.php">Create a new post!</a>
-<br>
+<br><br>
 <a href="edit-posts.php">Edit or delete previous posts.</a>
 </h2></center>
 
