c5bf4a6e837a06420309f6aa8f906823c40667d5
root <root@SCOOBY.localdomain>
2017-02-16 17:40:31 -0500

    Updated administrator.php so that it redirects non-admin users to their blog management page

--- administrate.php
+++ administrate.php
@@ -4,7 +4,7 @@ $_SESSION['old_page'] = 'administrate.php';
 include('requires/header.php');
 //User has logged in, but hasn't yet been authenticated as an admin
 if($LS->getUser("role") !== "admin")
-header('Location: '.$_SERVER['DOCUMENT_ROOT'].'/manage-posts.php');
+header('Location: Assignment%202/manage-posts.php');
 die();
 //If code below is executing then user can see page, i.e. successful login.
 //Make sure to clear the redirect var.
