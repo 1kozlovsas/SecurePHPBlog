688fa24ee5a2df41c08650d4956dacfda85c3829
root <root@SCOOBY.localdomain>
2017-02-16 13:05:16 -0500

    Updating new-posts.php with tinyMCE wysiwig editor (Froala is a pain to integrate)

--- new-post.php
+++ new-post.php
@@ -7,25 +7,10 @@ include('requires/header.php');
 $_SESSION['old_page'] = ''; 
 ?>
 
-<!-- Include Editor style. -->
-<link href='https://cdnjs.cloudflare.com/ajax/libs/froala-editor/2.4.2/css/froala_editor.min.css' rel='stylesheet' type='text/css' />
-<link href='https://cdnjs.cloudflare.com/ajax/libs/froala-editor/2.4.2/css/froala_style.min.css' rel='stylesheet' type='text/css' />
-<!-- Include JS file. -->
-<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/froala-editor/2.4.2/js/froala_editor.min.js'></script>
-<script
-  src="https://code.jquery.com/jquery-3.1.1.min.js"
-  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-  crossorigin="anonymous"></script>
+ <script src="//cloud.tinymce.com/stable/tinymce.min.js"></script>
+  <script>tinymce.init({ selector:'textarea' });</script>
 
-
-
-<textarea id="froala-editor">Initialize the Froala WYSIWYG HTML Editor on a textarea.</textarea>
-
-<script>
-  $(function() {
-    $('textarea#froala-editor').froalaEditor()
-  });
-</script>
+ <textarea>Easy! You should check out MoxieManager!</textarea>
 
 <?php
 include('requires/footer.php');
