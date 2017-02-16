8e4cd5754fa96c407d6d9c775069370a4597b3ef
root <root@SCOOBY.localdomain>
2017-02-16 12:08:24 -0500

    Updating manage-posts.php with some appropriate links, added some template pages for these new links

--- new-post.php
+++ new-post.php
@@ -1,15 +1,11 @@
 <?php
 session_start();
-$_SESSION['old_page'] = 'manage-posts.php';
+$_SESSION['old_page'] = 'new-post.php';
 include('requires/header.php');
 //If code below is executing then user can see page, i.e. successful login.
 //Make sure to clear the redirect var.
 $_SESSION['old_page'] = ''; 
 ?>
-
-<a href="new-post.php">Create a new post!</a>
-<br>
-<a href="edit-posts.php">Edit or delete previous posts.</a>
 
 <?php
 include('requires/footer.php');
