bf9304458d4a0851a50a699e31dcfab3e1ae8291
root <root@SCOOBY.localdomain>
2017-02-16 13:31:33 -0500

    Adding image handling to TinyMCE

--- new-post.php
+++ new-post.php
@@ -8,7 +8,12 @@ $_SESSION['old_page'] = '';
 ?>
 
  <script src="//cloud.tinymce.com/stable/tinymce.min.js?apiKey=usxf6kylya7uvf4cv4b5757vp65gi864icjj7guf9ojn43mi"></script>
-  <script>tinymce.init({ selector:'textarea' });</script>
+  <script>tinymce.init({
+  selector: "textarea",  // change this value according to your HTML
+  toolbar: "image",
+  plugins: "image imagetools",
+  imagetools_cors_hosts: ['mydomain.com', 'otherdomain.com']
+});</script>
 
  <textarea>Ayy lmao</textarea>
 
