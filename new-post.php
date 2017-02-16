4844305f07a2afa9f4e147c62dcf0560e1e36666
root <root@SCOOBY.localdomain>
2017-02-16 13:23:18 -0500

    Added API key to TinyMCE

--- new-post.php
+++ new-post.php
@@ -7,10 +7,10 @@ include('requires/header.php');
 $_SESSION['old_page'] = ''; 
 ?>
 
- <script src="//cloud.tinymce.com/stable/tinymce.min.js"></script>
+ <script src="//cloud.tinymce.com/stable/tinymce.min.js?apiKey=usxf6kylya7uvf4cv4b5757vp65gi864icjj7guf9ojn43mi"></script>
   <script>tinymce.init({ selector:'textarea' });</script>
 
- <textarea>Easy! You should check out MoxieManager!</textarea>
+ <textarea>Ayy lmao</textarea>
 
 <?php
 include('requires/footer.php');
