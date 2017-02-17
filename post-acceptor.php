ccefbc0d5008154324f9eb2ddc0b04e354aa6ed9
root <root@SCOOBY.localdomain>
2017-02-16 20:34:55 -0500

    Bug-hunting post-acceptor.php

--- post-acceptor.php
+++ post-acceptor.php
@@ -9,7 +9,10 @@ include('requires/config.php');
    * Change this line to set the upload folder *
    *********************************************/
   $imageFolder = "images/".$LS->getUser("username")."/";
-  mkdir(__DIR__."/".$imageFolder, 755);
+  if(!mkdir(__DIR__."/".$imageFolder, 755)){
+        header("HTTP/1.0 500 Permission denied.");
+        return;
+  }
 
   reset ($_FILES);
   $temp = current($_FILES);
