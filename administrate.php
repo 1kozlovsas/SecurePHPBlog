99c8f2da79bb80a25421cec26a6fbb8c90001ed3
1kozlovsas <sashak@cogeco.ca>
2017-02-22 11:49:38 -0800

    Added actual Admin Commands!

--- administrate.php
+++ administrate.php
@@ -4,13 +4,34 @@ $_SESSION['old_page'] = 'administrate.php';
 include('requires/header.php');
 //User has logged in, but hasn't yet been authenticated as an admin
 if($LS->getUser("role") !== "admin"){
-header('Location: manage-posts.php');
-die();
+header('Location: login.php');
+die("YOU ARE NOT ADMIN LEAVE THIS PLACE MORTAL");
 }
 //If code below is executing then user can see page, i.e. successful login.
 //Make sure to clear the redirect var.
 $_SESSION['old_page'] = ''; 
 ?>
+<h1>Welcome, Administrator.</h1>
+<br>
+<h2>Your command list awaits.</h2>
+<form action = "manage-account.php" method="POST">
+	<input type="text" name="username">Lookup a user's profile
+	<input type="submit" name="submit">
+</form>
+<br>
+<form action = "create-account.php" method="POST">
+	<input type="submit" name="submit">Create a new user
+</form>
+<br>
+<form action = "view-users.php" method="POST">
+	<input type="submit" name="submit">Check out a list of users
+</form>
+<br>
+<form action="delete-account.php">
+	<input type="text" name="username">Username to delete
+	<input type="submit" name="submit">Delete
+</form>
+	
 <?php
 include('requires/footer.php');
 ?>
