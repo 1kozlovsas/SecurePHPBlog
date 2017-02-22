075a93276439d3984512bd82f6f2bdd5ae4fd82e
1kozlovsas <sashak@cogeco.ca>
2017-02-22 13:00:04 -0800

    Fixing quotations

--- delete.php
+++ delete.php
@@ -22,9 +22,9 @@ pg_prepare($db, "query2","DELETE FROM posts WHERE username = $1");
 //$result = pg_execute($db, "query3", array("$username"));
 
 pg_prepare($db, "query3", "DELETE FROM user_devices AND user_tokens WHERE uid = $1");
-pg_execute($db, "query3", array("$LS->getUser("id")"));
-pg_execute($db, "query1", array("$username"));
-pg_execute($db, "query2", array("$username"));
+pg_execute($db, "query3", array($LS->getUser("id")));
+pg_execute($db, "query1", array($username));
+pg_execute($db, "query2", array($username));
 ?>
 
 
