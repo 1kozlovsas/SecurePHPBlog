6bf611f3e11b5e5fb1149b514ceb85a933dc9b33
root <root@SCOOBY.localdomain>
2017-02-22 17:32:55 -0500

    Working on forgot password functionality

--- activate.php
+++ activate.php
@@ -14,7 +14,7 @@ $password = "postgres";
 $connect = 'host=localhost dbname='.$dbname.' user='.$user.' password='.$password; 
 $db = pg_connect($connect);
 pg_prepare($db, "updateActivation","UPDATE users SET active = TRUE WHERE  activation_token = $1");
-pg_prepare($db, "deleteActivation","UPDATE users SET activation_token = "" WHERE  activation_token = $1");
+pg_prepare($db, "deleteActivation","UPDATE users SET activation_token = '' WHERE  activation_token = $1");
 pg_execute($db, "updateActivation", array($token));
 pg_execute($db, "deleteActivation", array($token));
 
