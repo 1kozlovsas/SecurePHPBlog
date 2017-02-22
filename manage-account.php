ad67ea29cb084fb0f131213f16d66266a0b0a6dc
1kozlovsas <sashak@cogeco.ca>
2017-02-22 13:18:10 -0800

    Making manage account great again

--- manage-account.php
+++ manage-account.php
@@ -69,19 +69,44 @@ echo $profpic;
 </form>
 
 <br>
-
-<h2>Change password(I recommend hunter2)</h2>
-<form action = "change-password.php" method="POST">
-
-		<label>
-			<input type="text" name="old_pass">Enter your current password.
-		</label>
-		<label>
-			<input type="text" name="new_pass">Enter your new password. It can be the same, but I wouldn't recommend it.
-		</label>
-		 <input type="submit" value="Change password pls" name="submit">
-
-</form>
+<?php
+    if(isset($_POST['change_password'])){
+      if(isset($_POST['current_password']) && $_POST['current_password'] != "" && isset($_POST['new_password']) && $_POST['new_password'] != "" && isset($_POST['retype_password']) && $_POST['retype_password'] != "" && isset($_POST['current_password']) && $_POST['current_password'] != ""){
+          
+        $curpass = $_POST['current_password'];
+        $new_password = $_POST['new_password'];
+        $retype_password = $_POST['retype_password'];
+          
+        if($new_password != $retype_password){
+          echo "<p><h2>Passwords Doesn't match</h2><p>The passwords you entered didn't match. Try again.</p></p>";
+        }else if($LS->login($LS->getUser("username"), $curpass, false, false) == false){
+          echo "<h2>Current Password Wrong!</h2><p>The password you entered for your account is wrong.</p>";
+        }else{
+          $change_password = $LS->changePassword($new_password);
+          if($change_password === true){
+            echo "<h2>Password Changed Successfully</h2>";
+          }
+        }
+      }else{
+        echo "<p><h2>Password Fields was blank</h2><p>Form fields were left blank</p></p>";
+      }
+    }
+    ?>
+    <form action="<?php echo $LS->curPageURL();?>" method='POST'>
+      <label>
+        <p>Current Password</p>
+        <input type='password' name='current_password' />
+      </label>
+      <label>
+        <p>New Password</p>
+        <input type='password' name='new_password' />
+      </label>
+      <label>
+        <p>Retype New Password</p>
+        <input type='password' name='retype_password' />
+      </label>
+      <button style="display: block;margin-top: 10px;" name='change_password' type='submit'>Change Password</button>
+    </form>
 
 
 <?php
