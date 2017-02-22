0199f62afbd0489e310a14719211e267e28e94a9
root <root@SCOOBY.localdomain>
2017-02-22 12:10:56 -0500

    Working on adding activation links to the login

---
+++ logintester.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+include('requires/header.php');
+?>
+      <?php
+      $two_step_login_active = false;
+      if($LS->twoStepLogin() === false && isset($_POST['action_login'])){
+        $identification = $_POST['login'];
+        $password = $_POST['password'];
+        if($identification == "" || $password == ""){
+            echo $LS->getActivationStatus($identification);
+            echo "<br>";
+            echo "<h2>Error</h2><p>Username hasn't been activated!</p>";
+        }
+      }
+      ?>
+      
+        <form action="login.php" method="POST" style="margin:0px auto;display:table;">
+          <label>
+            <p>Username / E-Mail</p>
+            <input name="login" type="text"/>
+          </label><br/>
+          <label>
+            <p>Password</p>
+            <input name="password" type="password"/>
+          </label><br/>
+          <label>
+            <p>
+              <input type="checkbox" name="remember_me"/> Remember Me
+            </p>
+          </label>
+          <div clear></div>
+          <button style="width:150px;" name="action_login">Log In</button>
+        </form>
+<?php
+include ('requires/footer.php');
+?>
