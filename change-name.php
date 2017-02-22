d44cf47cfe2059a4315770ebf5930d04d78f202b
1kozlovsas <sashak@cogeco.ca>
2017-02-22 11:04:58 -0800

    Added changing name functionality.

---
+++ change-name.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+include ('requires/header.php');
+?>
+<?php		
+$dbname = "";
+$user = "";
+$password = "";
+$username = $LS->getUser("username");
+$connect = 'host=localhost dbname='.$dbname.' user='.$user.' password='.$password; 
+$db = pg_connect($connect);
+pg_prepare($db, "query1","UPDATE TABLE users SET name = $1 WHERE username = $2");
+pg_execute($db, "query1", array("$_POST[new_name]", "$username"));
+
+?>
+
+
+<?php
+pg_close($db);
+include ('requires/footer.php');
+?>
