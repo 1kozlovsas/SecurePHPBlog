1a632c5ead0d827f88de21de81789970cb69d632
1kozlovsas <sashak@cogeco.ca>
2017-02-22 11:50:36 -0800

    Added function so only admin can view users

--- view-users.php
+++ view-users.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 include ('requires/header.php');
+if($LS->getUser("role") !== "admin"){
+header('Location: login.php');
+die("YOU ARE NOT ADMIN LEAVE THIS PLACE MORTAL");
+}
 ?>
 <h1>Pick a user. Any user. I dare you.</h1>
 
@@ -12,9 +16,9 @@ include ('requires/header.php');
 </thead>		
 <tbody>		 
 <?php
-$dbname = ""
-$user = ""
-$password = ""
+$dbname = "";
+$user = "";
+$password = "";
 $connect = 'host=localhost dbname='.$dbname.' user='.$user.' password='.$password; 
 $db = pg_connect($connect);
 $query = "SELECT username FROM users";             
