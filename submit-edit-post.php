6c16d13ec64e48001c2cd8ae42b42d808cd2c1e4
root <root@SCOOBY.localdomain>
2017-02-23 08:28:25 -0500

    Working on editing and deletion of posts

--- submit-edit-post.php
+++ submit-edit-post.php
@@ -3,9 +3,9 @@ session_start();
 include('requires/header.php');
 
 include ('requires/database-preamble.php');
-pg_prepare($db, "insertPost","UPDATE posts SET (body, date) = ($1, $2) WHERE id = $3");
+pg_prepare($db, "editPost","UPDATE posts SET (body, created) = ($1, $2) WHERE id = $3");
 
-pg_execute($db, "insertPost", array($_POST['post-html'], date("Y-m-d H:i:s"), $_POST['id']));
+pg_execute($db, "editPost", array($_POST['post-html'], date("Y-m-d H:i:s"), $_POST['id']));
 
 //echo $_POST['post-html'];
 
