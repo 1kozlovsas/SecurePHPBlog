dd51e0193929e6eae760e6f8b4fc49260a8d93aa
root <root@SCOOBY.localdomain>
2017-02-22 21:24:59 -0500

    Rearranging deletion order in delete.php to respect table constraints

--- delete.php
+++ delete.php
@@ -1,5 +1,5 @@
 <?php
-public static function delTree($dir) { 
+function delTree($dir) { 
    $files = array_diff(scandir($dir), array('.','..')); 
     foreach ($files as $file) { 
       (is_dir("$dir/$file")) ? delTree("$dir/$file") : unlink("$dir/$file"); 
