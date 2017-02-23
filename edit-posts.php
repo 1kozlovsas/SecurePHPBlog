79368581066b148824ae1cfbd5cfa3eb778c2cd7
root <root@SCOOBY.localdomain>
2017-02-23 07:49:43 -0500

    Working on editing and deletion of posts

--- edit-posts.php
+++ edit-posts.php
@@ -6,32 +6,45 @@ include('requires/header.php');
 
 <h1>LIST OF DETAILS:</h1>	
 <table>		
-<thead>			
-<tr>				
-<td>Username</td>				
-<td>Post:</td>				
-<td>Created on:</td>		
-</tr>		
-</thead>	
-<tbody>		
+    <thead>			
+        <tr>				
+            <td>Username</td>				
+            <td>Post:</td>				
+            <td>Created on:</td>
+            <td>Option:</td>
+        </tr>		
+    </thead>	
+    <tbody>		
 
-<?php	
-include ('requires/database-preamble.php');
-$res = pg_query_params($db, 'SELECT username, body, created FROM posts where username = $1',[$_GET["username"]]);	
-while($line = pg_fetch_row($res)){ ?>	
-<tr>		
-<?php foreach($line as $cell){ ?>	
-<td>	
-<?php print_r($cell);?> 
-</td>			
-<?php
-}	
-?> 	
-</tr>	
-<?php		
-}	
-?>	
-</tbody>	
+        <?php	
+        include ('requires/database-preamble.php');
+        $res = pg_query_params($db, 'SELECT id, username, body, created FROM posts where username = $1',[$_GET["username"]]);	
+        while($line = pg_fetch_row($res)){ ?>	
+        <tr>		
+            <td>	
+                <?php echo $line[1];?> //username
+            </td>	
+            <td>	
+                <?php echo $line[2];?> //body
+            </td>	
+            <td>	
+                <?php echo $line[3];?> //created
+            </td>	
+            <td>
+            <a href="edit-post.php?id=<?php echo $line[0];?>">                
+            <button type="button" class="btn btn-info">Edit Post
+            </button>                
+            </a>  
+            <a href="delete-post.php?id=<?php echo $line[0];?>">                
+            <button type="button" class="btn btn-info">Delete Post
+            </button>                
+            </a>   
+            </td>
+        </tr>	
+        <?php		
+        }	
+        ?>	
+    </tbody>	
 </table>
 <?php
 include ('requires/footer.php');
