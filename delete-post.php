c74a54afadff42f99b94a6c18d0c4cbb1147ad20
root <root@SCOOBY.localdomain>
2017-02-23 08:45:56 -0500

    Working on post deletion

--- delete-post.php
+++ delete-post.php
@@ -3,7 +3,7 @@ session_start();
 include('requires/header.php');
 include ('requires/database-preamble.php');
 
-$res = pg_query_params($db, 'SELECT username FROM posts where id = $1', [$_POST['id']]);
+$res = pg_query_params($db, 'SELECT username FROM posts where id = $1', [$_GET['id']]);
 $line = pg_fetch_row($res);
 if($line[0] !== $LS->getUser("username")){
     //User is trying to delete someone elses posts!
@@ -13,7 +13,7 @@ if($line[0] !== $LS->getUser("username")){
 
 pg_prepare($db, "deletePost","DROP posts WHERE id = $1");
 
-pg_execute($db, "deletePost", array($_POST['id']));
+pg_execute($db, "deletePost", array($_GET['id']));
 
 //echo $_POST['post-html'];
 
