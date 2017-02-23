06bf378daa870d438a429e6c539fa8412d61e96e
root <root@SCOOBY.localdomain>
2017-02-23 15:01:43 -0500

    Fixing profile display on view-posts

--- view-posts.php
+++ view-posts.php
@@ -30,7 +30,7 @@ Welcome to <?php echo $details[0];?>'s blog!
 <br>
 <img src="images/<?php echo $username;?>/avatar/<?php echo $profpic[2];?>" style="background-color: #ffffff; border-style:solid; border-width: 1px">
 <br>
-<p><?php echo $details[1];?></p>
+<pre><?php echo $details[1];?></pre>
 </div>
 </table>
 <br>
