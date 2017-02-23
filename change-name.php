b7a9dd39894e33de71a59a8adb61e6bb4158329a
root <root@SCOOBY.localdomain>
2017-02-23 14:19:21 -0500

    Finishing up manage-account.php

--- change-name.php
+++ change-name.php
@@ -5,7 +5,7 @@ include ('requires/header.php');
 <?php		
 include ('requires/database-preamble.php');
 pg_prepare($db, "query1","UPDATE users SET name = $1 WHERE username = $2");
-pg_execute($db, "query1", array("$_POST[new_name]", "$username"));
+pg_execute($db, "query1", array($_POST[new_name], $username));
 
 ?>
 
