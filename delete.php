78248af14c2a039330649b9a43824da888688ef9
root <root@SCOOBY.localdomain>
2017-02-22 21:19:46 -0500

    Enhancing delete.php

--- delete.php
+++ delete.php
@@ -1,4 +1,12 @@
 <?php
+public static function delTree($dir) { 
+   $files = array_diff(scandir($dir), array('.','..')); 
+    foreach ($files as $file) { 
+      (is_dir("$dir/$file")) ? delTree("$dir/$file") : unlink("$dir/$file"); 
+    } 
+    return rmdir($dir); 
+  } 
+
 session_start();
 include ('requires/header.php');
 if($LS->getUser("role") === "admin"){
@@ -7,8 +15,10 @@ $username = $_POST["username"];
 else{
 	$username = $LS->getUser("username");
 }
-?>
-<?php		
+
+
+
+
 $dbname = "postgres";
 $user = "postgres";
 $password = "postgres";
@@ -17,18 +27,16 @@ $connect = 'host=localhost dbname='.$dbname.' user='.$user.' password='.$passwor
 $db = pg_connect($connect);
 pg_prepare($db, "query1","DELETE FROM users WHERE username = $1");
 pg_prepare($db, "query2","DELETE FROM posts WHERE username = $1");
-//$result = pg_prepare($db, "query3","SELECT uid FROM user_devices WHERE username = $1");
-
-//$result = pg_execute($db, "query3", array("$username"));
-
-pg_prepare($db, "query3", "DELETE FROM user_devices AND user_tokens WHERE uid = $1");
-pg_execute($db, "query3", array($LS->getUser("id")));
+pg_prepare($db, "query3", "DELETE FROM user_devices WHERE uid = $1");
+pg_prepare($db, "query4", "DELETE FROM user_tokens WHERE uid = $1");
 pg_execute($db, "query1", array($username));
 pg_execute($db, "query2", array($username));
-?>
+pg_execute($db, "query3", array($LS->getUser("id")));
+pg_execute($db, "query4", array($LS->getUser("id")));
 
+delTree("images/".$username."/");
 
-<?php
+    
 pg_close($db);
 include ('requires/footer.php');
 ?>
