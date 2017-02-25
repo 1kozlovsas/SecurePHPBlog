4711c0f891b0e70a33012dcc7e9f964371a80b19
root <root@SCOOBY.localdomain>
2017-02-25 17:38:30 -0500

    Making view-recent.php

--- view-recent.php
+++ view-recent.php
@@ -3,11 +3,6 @@ session_start();
 include('requires/header.php');
 include ('requires/database-preamble.php');
 $res = pg_query($db, 'SELECT username, body, created FROM posts ORDER BY id DESC LIMIT 10');	
-
-$avatar_dir = "images/".$username."/avatar";
-
-$profpic = scandir($avatar_dir);
-
 ?>
 
 <link rel="stylesheet" href="css/blog.css"> <!-- Limits image size -->      
@@ -21,7 +16,10 @@ $profpic = scandir($avatar_dir);
 		
 
 <?php	
-while($line = pg_fetch_row($res)){ ?>
+while($line = pg_fetch_row($res)){ 
+    $avatar_dir = "images/".$line[0]."/avatar";
+    $profpic = scandir($avatar_dir);
+?>
 <tbody>
 <tr>		
 <td style="background-color: #eeeeee; border-style:solid; border-width: 2px">
