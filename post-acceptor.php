c907ec8b6fcff78bd4780204c6fbf86ecfe578d7
root <root@SCOOBY.localdomain>
2017-02-28 23:37:22 -0500

    Last minute touchups

--- post-acceptor.php
+++ post-acceptor.php
@@ -1,7 +1,24 @@
 <?php
 session_start();
 //include('requires/config.php'); //This is causing problems with JSON... :/
-include('requires/csrf.php');
+if(!empty($_POST['token'])) {
+
+    if (hash_equals($_SESSION['token'], $_POST['token'])) {
+
+	unset($_SESSION['token']);         
+
+    } else {
+
+	//if invalid token is provided.
+
+        die("CSRF DETECTED CSRF DETECTED");
+    }
+
+}
+else{
+	//May be a user refreshing, notify admin.
+	error_log("NOTICE: POSSIBLE CSRF ATTEMPT WITH ".$username."'s ACCOUNT-POST VARIABLE TOKEN IS EMPTY. OCCURRED AT ".$_SESSION['old_page']);
+}
   /*******************************************************
    * Only these origins will be allowed to upload images *
    ******************************************************/
