ae32127bf516cb9e286d4bd9399cb65a2040fae2
root <root@SCOOBY.localdomain>
2017-02-28 20:00:49 -0500

    Hardening administrate.php form, fixing post-acceptor, adding the CSRF protection to the header

--- requires/header.php
+++ requires/header.php
@@ -14,6 +14,30 @@ if(isset($_POST['action_login'])){
 		}
 	}
 }
+
+if(!empty($_POST['token'])) {
+
+    if (hash_equals($_SESSION['token'], $_POST['token'])) {
+
+	unset($_SESSION['token']);         
+
+    } else {
+
+	//if invalid token is provided.
+
+        die("CSRF DETECTED CSRF DETECTED");
+    }
+
+}
+else{
+	//May be a user refreshing, notify admin.
+	error_log("NOTICE: POSSIBLE CSRF ATTEMPT WITH ".$username."'s ACCOUNT-POST VARIABLE TOKEN IS EMPTY. OCCURRED AT ".$_SESSION['old_page']);
+}
+//generating new nonce and binding to session var
+$_SESSION['token'] = bin2hex(openssl_random_pseudo_bytes(32));
+
+$token = $_SESSION['token'];
+
 ?>
 
 <!doctype html>
