73e985a2e610722e696f76b6b17d5e388914a559
root <root@SCOOBY.localdomain>
2017-02-28 23:29:20 -0500

    Last minute touchups

--- change-profile.php
+++ change-profile.php
@@ -8,8 +8,9 @@ include('requires/header.php');
 if(isset($_POST['change_profile'])){
       if(isset($_POST['profile'])){          
           $LS->updateUser(array(
-            "profile" => $_POST['profile'],
-            $_SESSION['id'])
+            "profile" => $_POST['profile']),
+                          ,
+            $_SESSION['id']
           );
         }
     }
